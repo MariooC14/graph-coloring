592fa4501e37d9a790c8b872afdc49f88f100402
Mario C <mario.caval@icloud.com>
2026-03-21 17:31:39 +0000

    Added stochastic version of repair function

--- step1.py
+++ step1.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import random
 from networkx.classes import Graph
 
+# Set the seeds to `None` for "pure" randomness
 GRAPH_GEN_SEED = 2026
 COLORING_SEED = 2026
 NUM_COLORS = 3
@@ -32,9 +33,11 @@ def main():
 
     print(f"Initial number of conflicts: {count_conflicts(graph)}")
 
-    conflicts_over_time = repair_graph_coloring(graph, MAX_GRAPH_TRAVERSAL_ATTEMPTS)
+    # Change this line to use _v2 suffix for the stochastic version
+    conflicts_over_time = repair_graph_coloring_v1(graph, MAX_GRAPH_TRAVERSAL_ATTEMPTS)
 
     print(f"Final number of conflicts: {conflicts_over_time[-1]}")
+    print(f"Number of traversals: {len(conflicts_over_time)}")
 
     fig, ax = plt.subplots(1, 1)
     plot_graph(graph, ax, NUM_COLORS, 'Tutte Graph after Local Search Repair')
@@ -44,9 +47,11 @@ def main():
     plot_conflicts_over_time(conflicts_over_time)
 
 
-def repair_graph_coloring(graph: Graph, max_traversals = MAX_GRAPH_TRAVERSAL_ATTEMPTS):
+def repair_graph_coloring_v1(graph: Graph, max_traversals = MAX_GRAPH_TRAVERSAL_ATTEMPTS):
     '''
     Attempts to repair the graph coloring using a simple local search algorithm.
+    The color of a node is changed to the color that minimizes the number of conflicts with its neighbors.
+    For colors of equal minimum conflict, the one with the lowest index is picked.
     :return: The number of conflicts of the graph after each node traversed and changed.
     '''
     conflicts_over_time = []
@@ -71,6 +76,42 @@ def repair_graph_coloring(graph: Graph, max_traversals = MAX_GRAPH_TRAVERSAL_ATT
             graph.nodes[edge_idx]['color'] = color_to_change_to
 
     return conflicts_over_time
+
+
+def repair_graph_coloring_v2(graph: Graph, max_traversals = MAX_GRAPH_TRAVERSAL_ATTEMPTS):
+    '''
+    Attempts to repair the graph coloring using a simple local search algorithm.
+    The color of a node is changed to the color that minimizes the number of conflicts with its neighbors.
+    For colors of equal minimum conflict, the chosen color is picked at random.
+    :return: The number of conflicts of the graph after each node traversed and changed.
+    '''
+    conflicts_over_time = []
+    done = False
+    for i in range(max_traversals):
+        if done:
+            break
+        for edge_idx, neighbors_dict in graph.adjacency():
+            num_conflicts = count_conflicts(graph)
+            conflicts_over_time.append(num_conflicts)
+            if num_conflicts == 0:
+                print(f"Found a valid coloring!")
+                done = True             # Break out of the outer loop too
+                break
+            potential_conflicts  = [0 for _ in range(NUM_COLORS)]
+
+            for neighbor, neighbor_attrs in neighbors_dict.items():
+                nbr_color = graph.nodes[neighbor]['color']
+                potential_conflicts[nbr_color] += 1
+
+            smallest_conflicts = min(potential_conflicts)
+            colors_with_smallest_conflicts = [color for color, conflicts in enumerate(potential_conflicts) if conflicts == smallest_conflicts]
+            print(colors_with_smallest_conflicts)
+            color_to_change_to = random.choice(colors_with_smallest_conflicts)
+
+            graph.nodes[edge_idx]['color'] = color_to_change_to
+
+    return conflicts_over_time
+
 
 
 def color_graph_randomly(graph: Graph, num_colors = 3):
